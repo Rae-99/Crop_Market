165ba7a729ad253938567f202da99a7359ecff05
Rehan <rehanshikalgar2020@gmail.com>
2026-03-15 14:00:57 +0530

    Add helpdesk

--- Crop-Market.cpp
+++ Crop-Market.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 #include <unordered_map>
 
 using namespace std;
@@ -24,6 +24,7 @@ public:
 
             switch (a) {
                 case 1:
+                    Reenter:
                     cout << "Enter username: ";
                     cin >> username;
                     cout << "Enter password: ";
@@ -35,6 +36,7 @@ public:
                     } 
                     else {
                         cout << "Incorrect username and password pair\n";
+                        goto Reenter;
                     }
                     break;
 
@@ -79,7 +81,6 @@ public:
 };
 
 
-
 class buyer : public login {
     private:
         int amount, choice, quant;
@@ -106,7 +107,6 @@ class buyer : public login {
             else if(choice==0){
                 disp_cart();
                 disp_list();
-
             }
             
             //else display details about selected product
@@ -120,7 +120,6 @@ class buyer : public login {
 
 
         void transaction(){
-
 
             cout<<"Do you want to buy this product?\n(1. Yes 2. No) : ";
             cin>>temp1;
@@ -209,9 +208,7 @@ class buyer : public login {
             cout<<"Total Amount = "<<total<<"\n";
             
             }
-    
         } 
-
 };
 
 
@@ -251,10 +248,53 @@ public:
 };
 
 
+class Crop {
+private:
+    string name, soil, water, temperature, season;
+
+public:
+    Crop(string n, string s, string w, string t, string se)            //Constructor
+        : name(n), soil(s), water(w), temperature(t), season(se) {}
+
+    string getName() const {                                           //Getters
+        return name; 
+    }
+    string getSoil() const { 
+        return soil; 
+    }
+    string getWater() const { 
+        return water; 
+    }
+    string getTemperature() const {
+        return temperature; 
+    }
+    string getSeason() const { 
+        return season; 
+    }
+};
+
+int calculateScore(const Crop& c, const string& soil, const string& water,
+                   const string& temp, const string& season) {
+    int score = 0;
+
+    if (c.getSoil() == soil)   
+        score += 3;
+    if (c.getWater() == water)  
+        score += 2;
+    if (c.getTemperature() == temp) 
+        score += 2;
+    if (c.getSeason() == season) 
+        score += 3;
+
+    return score;
+}
+
+
+
 int main() {
     string username;
 
-    loginpage:              //Login
+    loginpage:              //LOGIN
     login l1;
     if(!l1.run())
         return 0;       //Exit Program
@@ -264,13 +304,13 @@ int main() {
 
     int k;
     frontpage:
-    cout<<"Buy or Sell ( 1. Buy    2. Sell  0. EXIT) : ";
+    cout<<"Buy or Sell ( 1. Buy     2. Sell     3. HelpDesk     0. EXIT ) : ";
     cin>>k;
 
     if(k==0)
         goto loginpage;
 
-    else if(k==1){          //BUY
+    else if(k==1){              //BUY
         buyer b1;
         restartb:
         b1.disp_list();
@@ -294,7 +334,7 @@ int main() {
     }
 
 
-    else if(k==2){            //SELL
+    else if(k==2){              //SELL
          sell s1;
          int ch;
         restarts:
@@ -322,6 +362,40 @@ int main() {
 
         }
         
+    }
+
+
+    else if(k==3){              //HELPDESK
+
+    vector<Crop> crops;
+    crops.push_back(Crop("Rice", "high", "high", "high", "monsoon"));               
+    crops.push_back(Crop("Wheat", "high", "moderate", "moderate", "winter"));
+    crops.push_back(Crop("Maize", "moderate", "moderate", "moderate", "summer"));
+    crops.push_back(Crop("Millets", "low", "low", "high", "summer"));
+    crops.push_back(Crop("Sugarcane", "high", "high", "high", "monsoon"));
+
+
+    
+    cout<<"Share your farm details and we'll help you get MAXIMUM YEILD!!\n";
+
+    string soil, water, temp, season;
+
+    cout << "Enter soil fertility: ";  
+    cin >> soil;
+    cout << "Enter water supply: ";    
+    cin >> water;
+    cout << "Enter temperature: ";     
+    cin >> temp;
+    cout << "Enter season: ";          
+    cin >> season;
+
+    cout << "\nRecommended Crops:\n";
+
+    for (const Crop& c : crops) {
+        int score = calculateScore(c, soil, water, temp, season);
+        if (score >= 6)
+            cout << c.getName() << " (Score: " << score << ")\n";
+    }
     }
 
 
